3cfa0dcea8a6c4dcfa4e0d719c9ac7df2f3c86d0
OmaGash <francislaxasantos@gmail.com>
2021-08-12 11:53:38 +0800

    Implemented a State Machine for characters
    - Issue #13  also resolved.

--- LUNAS Game Folder/characters/character.gd
+++ LUNAS Game Folder/characters/character.gd
@@ -1,16 +1,39 @@
-extends Node
+extends KinematicBody
+#All character classes will inherit this script.
+class_name Character, "res://characters/player/portrait.jpg"
 
+#This will define how heavy the character will be
+var _gravity: = 9.807
+#Jump force (pun intended)
+var _jump_force = 10
+#Character's flat movement speed
+var _speed: = 2.0
+#The character's max HP, we will have a 1 damage to all attacks with some exceptions.
+var _max_hp: = 3
+var current_hp:= _max_hp
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#The character's affiliation
+enum{
+	PLAYER, 
+	ENEMY
+}
 
+#Finite states
+enum _state{
+	IDLE,#For when the character is not moving.
+	MOVE,#For when the character is moving.
+	ATTACK,#For when the character is attacking.
+	DAMAGED,#For when the character receives damage.
+	ABNORMAL,#For when the character is stunned, slowed, etc.
+	ALERT#For NPC's only, when they go vigilante against the player.
+}
+#Default state will be idle
+var current_state = _state.IDLE setget _change_state
+var _previous_state = current_state
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#Changes the state if it's different from the current one
+func _change_state(_new_state):
+	if _new_state != current_state:
+		_previous_state = current_state#Save the last state to prev state.
+		current_state = _new_state
+		print(current_state)
--- LUNAS Game Folder/protopyte/player/player.gd
+++ LUNAS Game Folder/protopyte/player/player.gd
@@ -1,38 +1,44 @@
-extends KinematicBody
+extends Character
 
-#This will define how heavy the player will be
-export var gravity: = 9.807
-#Player's movement speed
-export var speed: = 2.0
 
 #Variable for storing velocity
 var velocity: = Vector3()
 #Which way is up
 var up: = Vector3(0,-1,0)
 
-#Finite states
-enum{
-	IDLE,
-	WALK
-}
-
-var current_state = IDLE
 
 func _ready():
-	pass 
+	translation.z = 0 
 
 func _physics_process(delta):
 	#Player will always be affected by gravity, regardless of which state they are currently in.
-	velocity.y -= gravity * delta
+	velocity.y -= _gravity * delta
 	#Check if current state matches any of the finite states
-	#Match works similar to switch statements
 	match current_state:
-		IDLE:
-			pass
-		WALK:
-			pass
-	velocity = move_and_slide(velocity, up)
+		_state.IDLE:
+			#Idle animation goes here.
+			#$anim.play("idle")
+			_move(delta)#You can only move if you're idle/moving
+		_state.MOVE:
+			_move(delta)
+		_state.DAMAGED:
+			 current_hp -= 1
+			#Damage indicator here
+	velocity = move_and_slide(velocity)
 
-#All movement-related code will go here
-func move(delta):
-	pass
+func _move(delta):
+	if Input.is_action_just_pressed("move_up"):
+		self.current_state = _state.MOVE
+		velocity.y = _jump_force
+		#This makes sure na the current_state will only change kapag current_state == _state.MOVE
+		self.current_state = _previous_state if current_state == _state.MOVE else current_state
+	if Input.is_action_pressed("move_left"):
+		self.current_state = _state.MOVE
+		velocity.x = -_speed
+	elif Input.is_action_pressed("move_right"):
+		self.current_state = _state.MOVE
+		velocity.x = _speed
+	else:
+		#This makes sure na the current_state will only change kapag current_state == _state.MOVE
+		self.current_state = _previous_state if current_state == _state.MOVE else current_state
+		velocity.x = 0#Stop the player from moving
