3bc93daee52384180a7af977b67b436e09237801
Raymund Paolo I. Pineda <78526690+Pa0paO@users.noreply.github.com>
2021-09-29 20:41:27 +0800

    animation_run_and_jumping

---
+++ game/environment/minigames/langit_lupa/npc.gd
@@ -0,0 +1,30 @@
+extends Character
+
+
+onready var player_node: Character = $"../player"
+
+var velocity: Vector3
+
+
+func _ready():
+	_gravity = 50
+	_speed =10
+
+func _physics_process(delta):
+	velocity.y -= _gravity
+	velocity.z = 0
+	translation.z=0
+	if translation.x < player_node.translation.x:
+		velocity.x = _speed
+	elif translation.x > player_node.translation.x:
+		velocity.x = -_speed
+	
+	
+	velocity=move_and_slide(velocity)
+	
+	_on_interact_body_entered(velocity)
+
+
+func _on_interact_body_entered(body):
+	for i in get_slide_count():
+		var collision=get_slide_collision(i)
