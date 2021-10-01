805efdf96035a4cab701ae92d6e055f7b133af43
Francis Santos <francislaxasantos@gmail.com>
2021-10-01 11:28:01 +0800

    Implemented game pausing and artifax system
    materials reimported for some reason
    
    changed resolution to 1366x768

--- game/environment/global.gd
+++ game/environment/global.gd
@@ -1,7 +1,27 @@
 extends Node
-#Global variables
+#Global variables and functions
+
+signal toggle_menu
+
 var player_name = "xXDesTr00yerXx" setget _change_name
+var artifact_passive = 3 #Passive unlocks
+var artifact_active = 2 #active unlocks
+var current_artifacts = [0, 0]
+
+func _ready():#This node will run regardless of pausing
+	pause_mode = Node.PAUSE_MODE_PROCESS
 
 func _change_name(new_name: String):
 	player_name = new_name
 	Dialogic.set_variable("player_name", new_name)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		pause()
+		emit_signal("toggle_menu", load("res://ui/artifax/artifax.tscn"))
+	if event.is_action_pressed("artifax"):
+		pause()
+		emit_signal("toggle_menu", load("res://ui/artifax/artifax.tscn"))
+
+func pause():
+	get_tree().paused = true if !get_tree().paused else false
