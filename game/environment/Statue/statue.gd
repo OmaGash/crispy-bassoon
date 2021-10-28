1bb43dd69a6de733911e945821f3678a8ebfc7f7
Francis Santos <francislaxasantos@gmail.com>
2021-10-28 16:12:22 +0800

    Earth statue done
    with animation

--- game/environment/Statue/statue.gd
+++ game/environment/Statue/statue.gd
@@ -1,5 +1,5 @@
 extends Area
-
+class_name Statue, "res://characters/concept/statue/statue-earth.png"
 export var link:NodePath
 
 onready var link_noded: = get_node(link)
@@ -16,19 +16,19 @@ func _interact(player):
 	if link_noded==null:
 		print(name + " linked node was not assigned.")
 		return
-	if is_off:
+	if is_off:#If statue is disabled
 		if player.hasoffer==true:
 			if link_noded.has_method("interact")==true:
 				is_interacting = true
-				print("asjdllasdj")
 				link_noded.call("interact", is_off)
-				is_off = false
+				is_off = false if not is_in_group("geo") else true
 				yield(link_noded, "done")
 				player.current_state=0
-				player.hasoffer = false
+				player.hasoffer = false if not is_in_group("geo") else true
 		else:
-			pass#Tell player that he needs an offer
-	else:
+			print("offer plz")
+			player.current_state = 0#Tell player that he needs an offer
+	else:#If statue is enabled
 		if player.hasoffer == false:
 			if link_noded.has_method("interact")==true:
 				is_interacting = true
@@ -36,7 +36,7 @@ func _interact(player):
 				is_off = true
 				yield(link_noded, "done")
 				player.current_state=0
-				player.hasoffer = true
+				player.hasoffer = true if not is_in_group("geo") else false
 		else:#If player already has offer
-			pass#Tell player that he cant have more than 1 offer at hand
+			print("dont be greedy")#Tell player that he cant have more than 1 offer at hand
 
