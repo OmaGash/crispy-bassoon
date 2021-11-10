b1c3b8b22b67dc4550437d084fdacdcad7773174
Raymund Paolo I. Pineda <78526690+Pa0paO@users.noreply.github.com>
2021-11-10 17:15:34 +0800

    pregame scene

---
+++ game/pre.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var next_scene="res://environment/minigames/langit_lupa/langit_lupa.tscn"
+var return_scene="res://ui/main_menu.tscn"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_start_pressed():
+	loader.load_scene(next_scene, self)
+
+
+func _on_cancel_pressed():
+	loader.load_scene(return_scene,self)
