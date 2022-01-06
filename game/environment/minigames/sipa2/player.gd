69aaae6b6db2c654e7a2bfeae76bc445475b5ced
malvedalim <55613252+malvedalim@users.noreply.github.com>
2022-01-06 22:58:05 +0800

    Sipa
    Animation Tree

--- game/environment/minigames/sipa2/player.gd
+++ game/environment/minigames/sipa2/player.gd
@@ -1,29 +1,39 @@
 extends Character
 var velocity: Vector3
 var speed = 10
+onready var anim_tree = $"anim-sipa/AnimationTree"
 
+func ready():
+	pass
+	
+	
 
 func _unhandled_input(event):
+	anim_tree['parameters/Strafe/blend_amount'] = 0.1
 	if event.is_action_pressed("ui_accept"):
 		$"seggs".disabled = false
-		$"armature kunware/anim".play("kick")
+		anim_tree['parameters/Shot/active'] = true
 		$"anim-sipa/AnimationPlayer".play("Kick-loop")
 		yield($"armature kunware/anim", "animation_finished")
 		$"seggs".disabled = true
-	
+		
 		
 
 func _physics_process(delta):
-
+	anim_tree['parameters/Move/blend_amount'] = 0
 	velocity.y -= 9.8
 	velocity.z = 0
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
-		$"anim-sipa/AnimationPlayer".play("Jog Strafe Left-loop")
+		anim_tree['parameters/Strafe/blend_amount'] = 0.9
+		anim_tree['parameters/Move/blend_amount'] = 1
+#		$"anim-sipa/AnimationPlayer".play("Jog Strafe Left-loop")
 		
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = speed
-		$"anim-sipa/AnimationPlayer".play("Jog Strafe Right-loop")
+		anim_tree['parameters/Strafe/blend_amount'] = 0.1
+		anim_tree['parameters/Move/blend_amount'] = 1
+#		$"anim-sipa/AnimationPlayer".play("Jog Strafe Right-loop")
 	else:
 		velocity.x = 0
 		
