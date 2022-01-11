7b84aa28c10cfac1ff889bf7dfc5d7f067b09138
Francis Santos <francislaxasantos@gmail.com>
2022-01-11 09:11:17 +0800

    Hotfix intro music not playing on export

--- game/tools/snippets.gd
+++ game/tools/snippets.gd
@@ -1,8 +1,8 @@
 extends Node
 #Just some handy code snippets
 
-var music_player:= AudioStreamPlayer.new()
-var _music_files = ["res://environment/bgm/main.ogg","res://environment/bgm/main_loop.ogg" ]
+onready var music_player: AudioStreamPlayer = AudioStreamPlayer.new()
+var _music_files = [preload("res://environment/bgm/main.ogg"),preload("res://environment/bgm/main_loop.ogg") ]
 
 #Get the world node
 func get_world(world_name = "world") -> Node:
@@ -10,19 +10,16 @@ func get_world(world_name = "world") -> Node:
 
 func _ready():
 	music_player.pause_mode = 2
-	var stream = AudioStream.new()
 	music_player.connect("finished", self, "next_song")
-	if File.new().file_exists(_music_files[0]):
-		var music = load(_music_files[0])
-		music_player.stream = music
-		music_player.play()
+	music_player.stream = _music_files[0]
+	music_player.play()
 	add_child(music_player)
 	music_player.bus = "bgm"
 	
 
 func next_song():
 	if g.in_game: return
-	music_player.stream = load(_music_files[1])
+	music_player.stream = _music_files[1]
 	music_player.play()
 
 func stop_main_theme():
