4e9b824e2b85fd8e2677e425322cd07b860c9127
Francis Santos <francislaxasantos@gmail.com>
2022-01-13 19:47:26 +0800

    Added a way to toggle model preview
    Previews can make the game lag frequently, sometimes unnecessarily.

--- game/ui/shop.gd
+++ game/ui/shop.gd
@@ -1,13 +1,15 @@
 extends Control
 onready var texts: = $"items/tab_mythical/VBoxContainer/HBoxContainer/Panel/VBoxContainer/ScrollContainer/texts"
 onready var preview: = $"items/tab_themes/VBoxContainer2/HBoxContainer/Panel/ScrollContainer/texts"
-onready var preview_label = $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer/preview_label"
+onready var preview_label = $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/preview_label"
 onready var bg_label = $"items/tab_themes/VBoxContainer2/HBoxContainer/VBoxContainer/bg_label"
 onready var bg = $"items/tab_themes/VBoxContainer2/HBoxContainer/VBoxContainer/bg"
 var fax: = preload("res://ui/fax.tscn")
 var preview_scn = preload("res://ui/theme.tscn")#try theme
 var preview_model = preload("res://ui/preview.tscn")
 var _last_theme_pressed: int
+var _last_item_pressed: int
+var enable_preview: bool = false#Previews can lag the game quite nicely
 
 func _ready():
 	theme = load(g.theme) as Theme
@@ -36,12 +38,13 @@ func _generate_button(item: int) -> Button:
 	return button
 
 func _listing_pressed(item_id: int):#Calls when an item on the shop is pressed
+	_last_item_pressed = item_id
 	if texts.get_child_count() > 0:#Removew everything before placing new nodes
 		for child in texts.get_children():
 			child.queue_free()
 	var title: = Label.new()
 	var description: = Label.new()
-	var price: = Label.new()
+	var price: = Label.new()  
 	var buy_button: = Button.new()
 	title.text = g.entries[item_id]["name"]
 	description.text = tr(g.entries[item_id]["description"])
@@ -49,19 +52,33 @@ func _listing_pressed(item_id: int):#Calls when an item on the shop is pressed
 	price.text = tr("Price" )+(":")+ str(g.entries[item_id]["price"]) + "○"
 	buy_button.connect("pressed", self, "_buy_pressed", [item_id, buy_button])
 	
-	if !g.entries[item_id]["owned"]:
+	if !g.entries[item_id]["owned"]:#Not owned yet
 		buy_button.text = tr("Buy")
 		preview_label.text = tr("model_locked")
 		if $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child_count() > 2:#Remove existing preview
 			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child(get_child_count()-1).queue_free()
+		
+		#Remove toggle
+		if $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer".has_node("toggle"):
+			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/toggle".queue_free()
 	else:
 		buy_button.text = tr("View")
 		preview_label.text = tr("model_unlocked")
 		if $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child_count() > 2:#Remove existing preview
 			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child(get_child_count()-1).queue_free()
-		var preview_scene: ViewportContainer = preview_model.instance()
-		preview_scene.object = load(g.entries[item_id]["model"])
-		$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".add_child(preview_scene)
+		
+		if enable_preview:
+			var preview_scene: ViewportContainer = preview_model.instance()#Add preview
+			preview_scene.object = load(g.entries[item_id]["model"])
+			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".add_child(preview_scene)
+		
+		#Preview toggle=======================================================
+		if not $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer".has_node("toggle"):
+			var preview_toggle: CheckButton = CheckButton.new()
+			preview_toggle.connect("toggled", self, "_on_CheckBox_toggled")
+			preview_toggle.name = "toggle"
+			preview_toggle.pressed = enable_preview
+			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer".add_child(preview_toggle)
 	
 	texts.add_child(title)
 	texts.add_child(price)
@@ -69,6 +86,8 @@ func _listing_pressed(item_id: int):#Calls when an item on the shop is pressed
 	if $"items/tab_mythical/VBoxContainer/HBoxContainer/Panel/VBoxContainer".get_child_count() > 1:#Delete button if it already exists.
 		$"items/tab_mythical/VBoxContainer/HBoxContainer/Panel/VBoxContainer".get_child($"items/tab_mythical/VBoxContainer/HBoxContainer/Panel/VBoxContainer".get_child_count() - 1).queue_free()
 	$"items/tab_mythical/VBoxContainer/HBoxContainer/Panel/VBoxContainer".add_child(buy_button)
+	
+	
 
 func _theme_pressed(item_id: int):
 	_last_theme_pressed = item_id
@@ -154,3 +173,16 @@ func _on_close_pressed():
 		get_parent().get_parent().get_node("player").current_state = 0
 		g.in_game = true
 	get_parent().toggle_menu(load("res://ui/shop.tscn"))
+
+
+func _on_CheckBox_toggled(button_pressed):#When the preview is toggled
+	enable_preview = button_pressed
+	if !button_pressed:
+		if $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child_count() > 2:
+			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child($"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child_count()-1).queue_free()
+	else:
+		if $"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".get_child_count() < 3:
+			var model = preview_model.instance()
+			model.object = load(g.entries[_last_item_pressed]["model"])
+			print(_last_item_pressed)
+			$"items/tab_mythical/VBoxContainer/HBoxContainer/VBoxContainer".add_child(model)
